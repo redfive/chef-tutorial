3d863df51ab6041d44ef4cd86ddee1370b191c65
John Gaunt <john.gaunt@rd.io>
2013-09-03 14:56:02 -0700

    Initial working version of the Berk/Chef/Vagrant tutorial

---
+++ README.md
@@ -0,0 +1,18 @@
+# myface cookbook
+
+# Requirements
+-Chef 11.04 or higher
+-Ubuntu 12.04
+
+# Usage
+Example cookbook
+
+# Attributes
+None (yet)
+
+# Recipes
+default.rb
+
+# Author
+
+Author:: redfive (john.gaunt@rd.io)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'myface'
+maintainer       'John Gaunt'
+maintainer_email 'john.gaunt@rd.io'
+license          'All rights reserved'
+description      'Installs/Configures myface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
