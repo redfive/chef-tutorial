c1058ec777f33da334277f7e3910b4c46bf5d7be
John Gaunt <john.gaunt@rd.io>
2013-09-03 16:33:41 -0700

    add user creation and a test

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -5,7 +5,12 @@ describe 'myface::default' do
   include Helpers::Myface
 
   # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  #it 'runs no tests by default' do
+  #end
+
+  # Check if a user has been created:
+  it "creates a service account for the myface daemon" do
+    user("myface").must_exist
   end
 
 end
